a804e87a3ff55f126ef4a288d1865de2ebe87040
Aronprivato23 <wksmh95y6g@privaterelay.appleid.com>
2026-08-08 16:11:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,3 @@
-const A=["./","./index.html","./app.js","./style.css","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open("ricette-v11").then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
